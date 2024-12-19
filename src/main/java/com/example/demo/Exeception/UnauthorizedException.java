24ca5577a9e31bf4828778565dea622374c91829
Adem meddeb <134943236+Adem-meddeb@users.noreply.github.com>
2024-12-19 22:17:47 +0100

    Delete src directory

--- src/main/java/com/example/demo/Exeception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Exeception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
