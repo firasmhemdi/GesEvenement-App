5373aaaf60a498f047f08d849b9a1fb8b2377bdb
Adem meddeb <134943236+Adem-meddeb@users.noreply.github.com>
2024-12-19 21:54:45 +0100

    Delete Backend directory

--- Backend/src/main/java/com/example/demo/Exeception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Exeception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
