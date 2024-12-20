2fda1aedb7da625172e0e5d7f6c1c171687023ae
Adem meddeb <134943236+Adem-meddeb@users.noreply.github.com>
2024-12-20 15:26:10 +0100

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Dossier de sortie pour Vercel
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:9098'
